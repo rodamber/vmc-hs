eaff1e88d6f601ff981277a383022ea92c68e3c5
rodamber <rodrigoambernardo@gmail.com>
2017-02-11 20:54:29 +0000

    Fixed another bug in swc

--- sat/PBEncoder.hs
+++ sat/PBEncoder.hs
@@ -21,7 +21,7 @@ swc (PB wxs k) = do
 
   return $
     [[- x i, s i j] | i <- [1..n], j <- [1..w i]]
-    ++ [[s 1 j] | j <- [w 1 + 1..k]]
+    ++ [[- s 1 j] | j <- [w 1 + 1..k]]
     ++ [[-s (i-1) j , s i j] | i <- [2..n], j <- [1..k]]
     ++ [[- x i, -s (i-1) j, s i (j + w i)] | i <- [2..n], j <- [1..k - w i]]
     ++ [[- x i, -s (i-1) (k + 1 - w i)] | i <- [2..n]]
